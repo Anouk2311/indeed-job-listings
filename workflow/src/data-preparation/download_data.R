32ade62b040bb591d6bc46838d11e4d078519c8f
GeorgianaHutanu <georgiana.hutanu96@gmail.com>
2021-03-18 13:53:48 +0100

    update download_data.R

--- workflow/src/data-preparation/download_data.R
+++ workflow/src/data-preparation/download_data.R
@@ -123,8 +123,81 @@ download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/m
 
 ## download datasets marketeer ------------------------------------------------
 # job postings
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/postings.csv",
+              '../../data/marketeer/marketeer-listings.csv')
 
-# job descriptions
+# job descriptions part 1
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_0_100.csv",
+              '../../data/marketeer/marketeer-descriptions-0-100.csv')
 
+# job descriptions part 2
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_100_200.csv",
+              '../../data/marketeer/marketeer-descriptions-100-200.csv')
 
+# job descriptions part 3
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_200_400.csv",
+              '../../data/marketeer/marketeer-descriptions-200-400.csv')
 
+# job descriptions part 4
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_400_600.csv",
+              '../../data/marketeer/marketeer-descriptions-400-600.csv')
+
+# job descriptions part 5
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_600_662.csv",
+              '../../data/marketeer/marketeer-descriptions-600-662.csv')
+
+# job descriptions part 6
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_662_700.csv",
+              '../../data/marketeer/marketeer-descriptions-662-700.csv')
+
+# job descriptions part 7
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_700_774.csv",
+              '../../data/marketeer/marketeer-descriptions-700-774.csv')
+
+# job descriptions part 8
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_774_800.csv",
+              '../../data/marketeer/marketeer-descriptions-774-800.csv')
+
+# job descriptions part 9
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_800_850.csv",
+              '../../data/marketeer/marketeer-descriptions-800-850.csv')
+
+# job descriptions part 10
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_850_900.csv",
+              '../../data/marketeer/marketeer-descriptions-850-900.csv')
+
+# job descriptions part 11
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_900_950.csv",
+              '../../data/marketeer/marketeer-descriptions-900-950.csv')
+
+# job descriptions part 12
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_950_1000.csv",
+              '../../data/marketeer/marketeer-descriptions-950-1000.csv')
+
+# job descriptions part 13
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_1000_1050.csv",
+              '../../data/marketeer/marketeer-descriptions-1000-1050.csv')
+
+# job descriptions part 14
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_1050_1100.csv",
+              '../../data/marketeer/marketeer-descriptions-1050-1100.csv')
+
+# job descriptions part 15
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_1100_1150.csv",
+              '../../data/marketeer/marketeer-descriptions-1100-1150.csv')
+
+# job descriptions part 16
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_1150_1200.csv",
+              '../../data/marketeer/marketeer-descriptions-1150-1200.csv')
+
+# job descriptions part 17
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_1200_1250.csv",
+              '../../data/marketeer/marketeer-descriptions-1200-1250.csv')
+
+# job descriptions part 18
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_1250_1300.csv",
+              '../../data/marketeer/marketeer-descriptions-1250-1300.csv')
+
+# job descriptions part 19
+download.file("https://raw.githubusercontent.com/Anouk2311/indeed-job-listings/main/scraped%20data/marketeer/descriptions_alternative1_1300_1320.csv",
+              '../../data/marketeer/marketeer-descriptions-1300-1320.csv')

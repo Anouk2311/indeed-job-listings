767874a0c4ed0fc55d65dcf476356486d207ed5b
Anouk Heemskerk <79919820+Anouk2311@users.noreply.github.com>
2021-03-20 10:02:53 +0100

    delete update input

--- workflow/src/data-preparation/update_input.R
+++
@@ -1,5 +0,0 @@
-# Copy the raw data into input folder 
-# This step really depends on how files are shared across the different stages (e.g. if whole pipeline
-# is on a single machine, could directly access data from data directory)
-file.copy("../../data/indeed/postings_13.03.21.csv","../../gen/data-preparation/input/postings_13.03.21.csv")
-file.copy("../../data/indeed/descriptions_13.03.21.csv","../../gen/data-preparation/input/descriptions_13.03.21.csv")

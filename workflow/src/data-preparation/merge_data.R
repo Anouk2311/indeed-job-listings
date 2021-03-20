3f33689a6ad187f23293b708a1819edff4a2a120
Anouk Heemskerk <79919820+Anouk2311@users.noreply.github.com>
2021-03-20 09:34:11 +0100

    removed duplicate codes
    some downloading data code lines were double

--- workflow/src/data-preparation/merge_data.R
+++ workflow/src/data-preparation/merge_data.R
@@ -5,6 +5,7 @@ library(dplyr)
 ## 1.1 data scientist ---------------------------------------------------------
 # listings
 ds_listings <- read.csv("../../data/data-scientist/data-scientist-listings.csv", sep=',')
+
 # descriptions
 ds_descriptions_1 <- read.csv("../../data/data-scientist/data-scientist-descriptions-1-50.csv", sep=',')
 ds_descriptions_2 <- read.csv("../../data/data-scientist/data-scientist-descriptions-51-100.csv", sep=',')
@@ -32,42 +33,6 @@ ds_descriptions_21 <- read.csv("../../data/data-scientist/data-scientist-descrip
 # listings
 da_listings <- read.csv("../../data/data-analist/data-analist-listings.csv", sep=',')
 
-#descriptions
-da_descriptions_1 <- read.csv("../../data/data-analist/data-analist-descriptions-0-162.csv", sep=',')
-da_descriptions_2 <- read.csv("../../data/data-analist/data-analist-descriptions-162-220.csv", sep=',')
-da_descriptions_3 <- read.csv("../../data/data-analist/data-analist-descriptions-220-300.csv", sep=',')
-da_descriptions_4 <- read.csv("../../data/data-analist/data-analist-descriptions-300-400.csv", sep=',')
-da_descriptions_5 <- read.csv("../../data/data-analist/data-analist-descriptions-400-500.csv", sep=',')
-da_descriptions_6 <- read.csv("../../data/data-analist/data-analist-descriptions-500-600.csv", sep=',')
-da_descriptions_7 <- read.csv("../../data/data-analist/data-analist-descriptions-600-656.csv", sep=',')
-da_descriptions_8 <- read.csv("../../data/data-analist/data-analist-descriptions-656-719.csv", sep=',')
-da_descriptions_9 <- read.csv("../../data/data-analist/data-analist-descriptions-719-784.csv", sep=',')
-da_descriptions_10 <- read.csv("../../data/data-analist/data-analist-descriptions-784-847.csv", sep=',')
-da_descriptions_11 <- read.csv("../../data/data-analist/data-analist-descriptions-847-900.csv", sep=',')
-da_descriptions_12 <- read.csv("../../data/data-analist/data-analist-descriptions-900-958.csv", sep=',')
-da_descriptions_13 <- read.csv("../../data/data-analist/data-analist-descriptions-958-1020.csv", sep=',')
-da_descriptions_14 <- read.csv("../../data/data-analist/data-analist-descriptions-1020-1080.csv", sep=',')
-da_descriptions_15 <- read.csv("../../data/data-analist/data-analist-descriptions-1080-1144.csv", sep=',')
-da_descriptions_16 <- read.csv("../../data/data-analist/data-analist-descriptions-1144-1201.csv", sep=',')
-da_descriptions_17 <- read.csv("../../data/data-analist/data-analist-descriptions-1201-1223.csv", sep=',')
-
-## 1.3 marketing analist ------------------------------------------------------
-# listings
-ma_listings <- read.csv("../../data/marketing-analist/marketing-analist-listings.csv", sep=',')
-
-# descriptions
-ma_descriptions_1 <- read.csv("../../data/marketing-analist/marketing-analist-descriptions-1.csv", sep=',')
-ma_descriptions_2 <- read.csv("../../data/marketing-analist/marketing-analist-descriptions-145-250.csv", sep=',')
-ma_descriptions_3 <- read.csv("../../data/marketing-analist/marketing-analist-descriptions-225-300.csv", sep=',')
-ma_descriptions_4 <- read.csv("../../data/marketing-analist/marketing-analist-descriptions-300-375.csv", sep=',')
-ma_descriptions_5 <- read.csv("../../data/marketing-analist/marketing-analist-descriptions-375-450.csv", sep=',')
-ma_descriptions_6 <- read.csv("../../data/marketing-analist/marketing-analist-descriptions-450-510.csv", sep=',')
-ma_descriptions_7 <- read.csv("../../data/marketing-analist/marketing-analist-descriptions-510.csv", sep=',')
-
-## 1.4 marketeer --------------------------------------------------------------
-# listings
-m_listings <- read.csv("../../data/marketeer/marketeer-listings.csv", sep=',')
-
 # descriptions
 da_descriptions_1 <- read.csv("../../data/data-analist/data-analist-descriptions-0-162.csv", sep=',')
 da_descriptions_2 <- read.csv("../../data/data-analist/data-analist-descriptions-162-220.csv", sep=',')
@@ -105,12 +70,11 @@ ma_descriptions_7 <- read.csv("../../data/marketing-analist/marketing-analist-de
 m_listings <- read.csv("../../data/marketeer/marketeer-listings.csv", sep=',')
 
 # descriptions
-# I think there is an error with the 5th file that carries over to the next steps as well for marketeer data
 m_descriptions_1 <- read.csv("../../data/marketeer/marketeer-descriptions-0-100.csv", sep=',')
 m_descriptions_2 <- read.csv("../../data/marketeer/marketeer-descriptions-100-200.csv", sep=',')
 m_descriptions_3 <- read.csv("../../data/marketeer/marketeer-descriptions-200-400.csv", sep=',')
 m_descriptions_4 <- read.csv("../../data/marketeer/marketeer-descriptions-400-600.csv", sep=',')
-m_descriptions_5 <- read.csv("../../data/marketeer/marketeer-descriptions-600-662.csv", sep=',')
+m_descriptions_5 <- read.csv("../../data/marketeer/marketeer-descriptions-600-662.csv", sep=',')  # error with separator
 m_descriptions_6 <- read.csv("../../data/marketeer/marketeer-descriptions-662-700.csv", sep=',')
 m_descriptions_7 <- read.csv("../../data/marketeer/marketeer-descriptions-700-774.csv", sep=',')
 m_descriptions_8 <- read.csv("../../data/marketeer/marketeer-descriptions-774-800.csv", sep=',')

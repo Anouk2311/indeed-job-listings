6ed8d644b043f15c37cc096a32c985cc5a508823
Anouk Heemskerk <79919820+Anouk2311@users.noreply.github.com>
2021-03-20 10:01:27 +0100

    add clean data file
    renamed the transform file to make it clearer

---
+++ workflow/src/data-preparation/clean_data.R
@@ -0,0 +1,44 @@
+### load packages
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+### 1. load datasets into R ---------------------------------------------------
+## 1.1 data scientist ---------------------------------------------------------
+data_scientist <- read.csv('../../gen/data-preparation/output/data_scientist.csv', sep = ',')
+
+## 1.2 data analist -----------------------------------------------------------
+data_analist <- read.csv('../../gen/data-preparation/output/data_analist.csv', sep = ',')
+
+## 1.3 marketing analist ------------------------------------------------------
+marketing_analist <- read.csv('../../gen/data-preparation/output/marketing_analist.csv', sep = ',')
+
+## 1.4 marketeer --------------------------------------------------------------
+marketeer <- read.csv('../../gen/data-preparation/output/marketeer.csv', sep = ',')
+
+
+### 2. clean data ------------------------------------------------------------
+## 2.1 data scientist ---------------------------------------------------------
+
+
+## 2.2 data analist -----------------------------------------------------------
+
+
+## 2.3 marketing analist ------------------------------------------------------
+
+
+## 2.4 marketeer --------------------------------------------------------------
+
+
+### 3. save cleaned data -----------------------------------------------------
+## 3.1 data scientist ---------------------------------------------------------
+
+
+## 3.2 data analist -----------------------------------------------------------
+
+
+## 3.3 marketing analist ------------------------------------------------------
+
+
+## 3.4 marketeer --------------------------------------------------------------
+

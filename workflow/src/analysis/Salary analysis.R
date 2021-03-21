d62ff065cb8aa1ec6475bcc7b29b01aac7acd7ec
GeorgianaHutanu <georgiana.hutanu96@gmail.com>
2021-03-21 17:28:37 +0100

    update salary analysis.R

--- workflow/src/analysis/Salary analysis.R
+++ workflow/src/analysis/Salary analysis.R
@@ -1,48 +1,245 @@
+### 1. load datasets into R ---------------------------------------------------
+## 1.1 data scientist ---------------------------------------------------------
+data_scientist <- read.csv('../../gen/data-preparation/output/data_scientist_clean.csv', header=TRUE, row.names = "X", sep = ',')
 
-randstadsteden <- c("Amsterdam", "Utrecht", "Rotterdam", "Den Haag", "Amstelveen", "Schiphol", "Delft", "Amersfoort", "Almere", "Zaanstad", "Zaandam", "Zoetermeer", "Dordrecht", "Leiden", "Haarlemmermeer", "Nieuwegein", "Alphen aan de Rijn", "Westland", "Alkmaar", "Purmerend", "Schiedam", "Hoorn", "Vlaardingen", "Gouda")
-df_new <- df %>% mutate(randstad = ifelse (df$location %in% randstadsteden, 1,  0))
-df_new$randstad <- df_new$df$randstad <- ifelse (df$location %in% randstadsteden, 1,  0)
+## 1.2 data analist -----------------------------------------------------------
+data_analist <- read.csv('../../gen/data-preparation/output/data_analist_clean.csv', header=TRUE, row.names = "X", sep = ',')
 
-t.test <- t.test(df_new$output ~ df_new$randstad, mu=0, alt = "two.sided", conf = 0.95, var.eq = F, paired = F)
+## 1.3 marketing analist ------------------------------------------------------
+marketing_analist <- read.csv('../../gen/data-preparation/output/marketing_analist_clean.csv', header=TRUE, row.names = "X", sep = ',')
+
+## 1.4 marketeer --------------------------------------------------------------
+marketeer <- read.csv('../../gen/data-preparation/output/marketeer_clean.csv', header=TRUE, row.names = "X",  sep = ',')
+
+## 2.1 data scientist ---------------------------------------------------------
+# 2.1.1 clean salary column, which has either hourly, monthly or yearly values. Convert values into yearly salary based on the number of working hours in a year for an average job
+# Note: in many instances, the salary is given as a range. The new output will represent the lower bound of this range: min_salary
+
+df_ds <- data_scientist %>% 
+  mutate(salary1 = str_remove_all(salary, '[^0-9-,â,¬]+')) %>% 
+  separate(salary1, into = c('salary1', 'salary2'), 
+           convert = TRUE, extra = 'drop') %>%
+  mutate(across(c(salary1, salary2),
+                ~ case_when(str_detect(salary, "per maand") ~ . * 12, 
+                            str_detect(salary, 'per uur') ~ . * 40 * 4 * 12, 
+                            nchar(.) < 5 ~ as.numeric(str_pad(., pad = '0', 
+                                                              side = 'right', width = 5)),
+                            TRUE ~ as.numeric(.)))) %>% 
+  transmute(min_salary = rowMeans(select(., salary1, salary2), na.rm = TRUE))
+
+# join the two data frames
+data_scientist <- cbind(data_scientist, df_ds)
+
+# 2.1.2 discard the column X.1 as it is a duplicate of the index column 
+data_scientist$X.1 <- NULL
+
+
+## 2.2 data analist -----------------------------------------------------------
+# 2.2.1 clean salary column, which has either hourly, monthly or yearly values. Convert values into yearly salary based on the number of working hours in a year for an average job
+# Note: in many instances, the salary is given as a range. The new output will represent the lower bound of this range: min_salary
+
+df_da <- data_analist %>% 
+  mutate(salary1 = str_remove_all(salary, '[^0-9-,â,¬]+')) %>% 
+  separate(salary1, into = c('salary1', 'salary2'), 
+           convert = TRUE, extra = 'drop') %>%
+  mutate(across(c(salary1, salary2),
+                ~ case_when(str_detect(salary, "per maand") ~ . * 12, 
+                            str_detect(salary, 'per uur') ~ . * 40 * 4 * 12, 
+                            nchar(.) < 5 ~ as.numeric(str_pad(., pad = '0', 
+                                                              side = 'right', width = 5)),
+                            TRUE ~ as.numeric(.)))) %>% 
+  transmute(min_salary = rowMeans(select(., salary1, salary2), na.rm = TRUE))
+
+# join the two data frames
+data_analist <- cbind(data_analist, df_da)
+
+# 2.2.2 discard the column X.1 as it is a duplicate of the index column 
+data_analist$X.1 <- NULL
+
+
+## 2.3 marketing analist ------------------------------------------------------
+# 2.3.1 clean salary column, which has either hourly, monthly or yearly values. Convert values into yearly salary based on the number of working hours in a year for an average job
+# Note: in many instances, the salary is given as a range. The new output will represent the lower bound of this range: min_salary
+
+df_ma <- marketing_analist %>% 
+  mutate(salary1 = str_remove_all(salary, '[^0-9-,â,¬]+')) %>% 
+  separate(salary1, into = c('salary1', 'salary2'), 
+           convert = TRUE, extra = 'drop') %>%
+  mutate(across(c(salary1, salary2),
+                ~ case_when(str_detect(salary, "per maand") ~ . * 12, 
+                            str_detect(salary, 'per uur') ~ . * 40 * 4 * 12, 
+                            nchar(.) < 5 ~ as.numeric(str_pad(., pad = '0', 
+                                                              side = 'right', width = 5)),
+                            TRUE ~ as.numeric(.)))) %>% 
+  transmute(min_salary = rowMeans(select(., salary1, salary2), na.rm = TRUE))
+
+# join the two data frames
+marketing_analist <- cbind(marketing_analist, df_ma)
+
+# 2.3.2 discard the column X.1 as it is a duplicate of the index column 
+marketing_analist$X.1 <- NULL
+
+
+## 2.4 marketeer --------------------------------------------------------------
+# 2.4.1 clean salary column, which has either hourly, monthly or yearly values. Convert values into yearly salary based on the number of working hours in a year for an average job
+# Note: in many instances, the salary is given as a range. The new output will represent the lower bound of this range: min_salary
+
+df_m <- marketeer %>% 
+  mutate(salary1 = str_remove_all(salary, '[^0-9-,â,¬]+')) %>% 
+  separate(salary1, into = c('salary1', 'salary2'), 
+           convert = TRUE, extra = 'drop') %>%
+  mutate(across(c(salary1, salary2),
+                ~ case_when(str_detect(salary, "per maand") ~ . * 12, 
+                            str_detect(salary, 'per uur') ~ . * 40 * 4 * 12, 
+                            nchar(.) < 5 ~ as.numeric(str_pad(., pad = '0', 
+                                                              side = 'right', width = 5)),
+                            TRUE ~ as.numeric(.)))) %>% 
+  transmute(min_salary = rowMeans(select(., salary1, salary2), na.rm = TRUE))
 
 
 
-df_new = subset(df_new, select = -c(df))
 
-library(dplyr)
-library(stringr)
-df <- df %>%
-  mutate(location_trimmed = str_remove(location, "-Zuidoost"))
-words_to_remove <- c("-Zuidoost", "Centrum", "noord", "zuid", "Zuid", "Noord", "Binnenstad", "-Zuid", "-Noord", "")   
-df <- df %>%
-  mutate(location_trimmed = str_remove_all(location, regex(str_c(words_to_remove, collapse = '|'), ignore_case = T)))
+df_ds <- data_scientist %>% 
+  mutate(salary1 = str_remove_all(salary, '[^0-9-,â,¬]+')) %>% 
+  separate(salary1, into = c('salary1', 'salary2'), 
+           convert = TRUE, extra = 'drop') %>%
+  mutate(across(c(salary1, salary2),
+                ~ case_when(str_detect(salary, "per maand") ~ . * 12, 
+                            str_detect(salary, 'per uur') ~ . * 40 * 4 * 12, 
+                            nchar(.) < 5 ~ as.numeric(str_pad(., pad = '0', 
+                                                              side = 'right', width = 5)),
+                            TRUE ~ as.numeric(.)))) %>% 
+  transmute(min_salary = rowMeans(select(., salary1, salary2), na.rm = TRUE))
 
-df <- df %>%
-  mutate(location_trimmed = str_replace_all(location_trimmed, "Holland-", "Rotterdam"))
+# join the two data frames
+data_scientist <- cbind(data_scientist, df_ds)
+
+# 2.1.2 discard the column X.1 as it is a duplicate of the index column 
+data_scientist$X.1 <- NULL
 
 
-df <- df %>% 
-  mutate(location_trimmed = str_replace_all(location_trimmed, "Amsterdam ", "Amsterdam"))
+## 2.2 data analist -----------------------------------------------------------
+# 2.2.1 clean salary column, which has either hourly, monthly or yearly values. Convert values into yearly salary based on the number of working hours in a year for an average job
+# Note: in many instances, the salary is given as a range. The new output will represent the lower bound of this range: min_salary
 
-df <- df %>% 
-  mutate(location_trimmed = str_replace_all(location_trimmed, "Schiphol", "Amsterdam"))
+df_da <- data_analist %>% 
+  mutate(salary1 = str_remove_all(salary, '[^0-9-,â,¬]+')) %>% 
+  separate(salary1, into = c('salary1', 'salary2'), 
+           convert = TRUE, extra = 'drop') %>%
+  mutate(across(c(salary1, salary2),
+                ~ case_when(str_detect(salary, "per maand") ~ . * 12, 
+                            str_detect(salary, 'per uur') ~ . * 40 * 4 * 12, 
+                            nchar(.) < 5 ~ as.numeric(str_pad(., pad = '0', 
+                                                              side = 'right', width = 5)),
+                            TRUE ~ as.numeric(.)))) %>% 
+  transmute(min_salary = rowMeans(select(., salary1, salary2), na.rm = TRUE))
 
-df <- df %>% 
-  mutate(location_trimmed = str_replace_all(location_trimmed, "Randstad", "Amsterdam"))
+# join the two data frames
+data_analist <- cbind(data_analist, df_da)
 
-df <- df %>% 
-  mutate(location_trimmed = str_replace_all(location_trimmed, "Nederland", "Amsterdam"))
+# 2.2.2 discard the column X.1 as it is a duplicate of the index column 
+data_analist$X.1 <- NULL
 
-average_salaries <- df %>% 
+
+## 2.3 marketing analist ------------------------------------------------------
+# 2.3.1 clean salary column, which has either hourly, monthly or yearly values. Convert values into yearly salary based on the number of working hours in a year for an average job
+# Note: in many instances, the salary is given as a range. The new output will represent the lower bound of this range: min_salary
+
+df_ma <- marketing_analist %>% 
+  mutate(salary1 = str_remove_all(salary, '[^0-9-,â,¬]+')) %>% 
+  separate(salary1, into = c('salary1', 'salary2'), 
+           convert = TRUE, extra = 'drop') %>%
+  mutate(across(c(salary1, salary2),
+                ~ case_when(str_detect(salary, "per maand") ~ . * 12, 
+                            str_detect(salary, 'per uur') ~ . * 40 * 4 * 12, 
+                            nchar(.) < 5 ~ as.numeric(str_pad(., pad = '0', 
+                                                              side = 'right', width = 5)),
+                            TRUE ~ as.numeric(.)))) %>% 
+  transmute(min_salary = rowMeans(select(., salary1, salary2), na.rm = TRUE))
+
+# join the two data frames
+marketing_analist <- cbind(marketing_analist, df_ma)
+
+# 2.3.2 discard the column X.1 as it is a duplicate of the index column 
+marketing_analist$X.1 <- NULL
+
+
+## 2.4 marketeer --------------------------------------------------------------
+# 2.4.1 clean salary column, which has either hourly, monthly or yearly values. Convert values into yearly salary based on the number of working hours in a year for an average job
+# Note: in many instances, the salary is given as a range. The new output will represent the lower bound of this range: min_salary
+
+df_m <- marketeer %>% 
+  mutate(salary1 = str_remove_all(salary, '[^0-9-,â,¬]+')) %>% 
+  separate(salary1, into = c('salary1', 'salary2'), 
+           convert = TRUE, extra = 'drop') %>%
+  mutate(across(c(salary1, salary2),
+                ~ case_when(str_detect(salary, "per maand") ~ . * 12, 
+                            str_detect(salary, 'per uur') ~ . * 40 * 4 * 12, 
+                            nchar(.) < 5 ~ as.numeric(str_pad(., pad = '0', 
+                                                              side = 'right', width = 5)),
+                            TRUE ~ as.numeric(.)))) %>% 
+  transmute(min_salary = rowMeans(select(., salary1, salary2), na.rm = TRUE))
+
+
+
+
+#average salaries per location for data scientists
+
+average_salaries_ds <- df_ds %>% 
   group_by(location_trimmed) %>%                 
   summarise(average_salary = mean(output)) 
 
-highest_average_salary_cities <- average_salaries %>%
-  filter(average_salaries$average_salary > 50000)
+#filter cities with over 50000 euro's average yearly salary
+highest_average_salary_cities_ds <- average_salaries_ds %>%
+  filter(average_salaries_ds$average_salary > 50000)
+#build a plot of the locations with the highest average salary in the Netherlands
+plot_highest_salary_by_location_ds <- ggplot(highest_average_salary_cities_ds, aes(x = location_trimmed, y = average_salary)) + geom_bar(stat='identity') + coord_flip()
+plot_highest_salary_by_location_ds
 
-plot_highest_salary_by_location <- ggplot(highest_average_salary_cities, aes(x = location_trimmed, y = average_salary)) + geom_bar(stat='identity') + coord_flip()
-plot_highest_salary_by_location
+
+
+#average salaries per location for data analists
+
+average_salaries_da <- df_da %>% 
+  group_by(location_trimmed) %>%                 
+  summarise(average_salary = mean(output)) 
+
+#filter cities with over 50000 euro's average yearly salary
+highest_average_salary_cities_da <- average_salaries_da %>%
+  filter(average_salaries_da$average_salary > 50000)
+#filter cities with over 50000 euro's average yearly salary
+plot_highest_salary_by_location_da <- ggplot(highest_average_salary_cities_da, aes(x = location_trimmed, y = average_salary)) + geom_bar(stat='identity') + coord_flip()
+plot_highest_salary_by_location_da
 
 
 
 
+#average salaries per location for marketing analists
+
+average_salaries_ma <- df_ma %>% 
+  group_by(location_trimmed) %>%                 
+  summarise(average_salary = mean(output)) 
+
+#filter cities with over 50000 euro's average yearly salary
+highest_average_salary_cities_ma <- average_salaries_ma %>%
+  filter(average_salaries_ma$average_salary > 50000)
+#filter cities with over 50000 euro's average yearly salary
+plot_highest_salary_by_location_ma <- ggplot(highest_average_salary_cities_ma, aes(x = location_trimmed, y = average_salary)) + geom_bar(stat='identity') + coord_flip()
+plot_highest_salary_by_location_ma
+
+
+
+
+#average salaries per location for marketeers
+
+
+average_salaries_m <- df_m %>% 
+  group_by(location_trimmed) %>%                 
+  summarise(average_salary = mean(output)) 
+
+#filter cities with over 50000 euro's average yearly salary
+highest_average_salary_cities_m <- average_salaries_m %>%
+  filter(average_salaries_m$average_salary > 50000)
+#filter cities with over 50000 euro's average yearly salary
+plot_highest_salary_by_location_m <- ggplot(highest_average_salary_cities_m, aes(x = location_trimmed, y = average_salary)) + geom_bar(stat='identity') + coord_flip()
+plot_highest_salary_by_location_m
